cb3bcd0e325f60069ecfe4c92ce2430ab51580a0
Viktoras Laukevicius <viktoras.laukevicius@yahoo.com>
2016-08-05 15:45:33 +0300

    Initial Commit

---
+++ FuzzySearch/FuzzySearch.h
@@ -0,0 +1,19 @@
+//
+//  FuzzySearch.h
+//  FuzzySearch
+//
+//  Created by Viktoras Laukevičius on 05/08/16.
+//  Copyright © 2016 Treatwell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FuzzySearch.
+FOUNDATION_EXPORT double FuzzySearchVersionNumber;
+
+//! Project version string for FuzzySearch.
+FOUNDATION_EXPORT const unsigned char FuzzySearchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FuzzySearch/PublicHeader.h>
+
+
---
+++ FuzzySearchTests/FuzzySearchTests.swift
@@ -0,0 +1,36 @@
+//
+//  FuzzySearchTests.swift
+//  FuzzySearchTests
+//
+//  Created by Viktoras Laukevičius on 05/08/16.
+//  Copyright © 2016 Treatwell. All rights reserved.
+//
+
+import XCTest
+@testable import FuzzySearch
+
+class FuzzySearchTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
